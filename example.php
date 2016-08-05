bcbd64507e98c56d9cc41dfae313d339fa6ff5b0
Teerasark <teerasark@yahoo.com>
2016-08-05 15:55:41 +0700

    1

---
+++ example.php
@@ -0,0 +1,30 @@
+<?php
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, 'http://it.cmtc.ac.th/web_service/test6_teq/test6.json');
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); 
+   
+    $exec = curl_exec($curl);
+    curl_close($curl);
+
+    //print_r(json_decode($exec));
+	$data=json_decode($exec);
+
+	foreach ($data as $key => $value)
+	{
+	    echo 'ID - '. $value->_id;
+		echo '<br>';
+		echo 'RECV - '. $value->_rev;
+		echo '<br>';
+		echo 'ระยะเวลา - '. $value->Period;
+		echo '<br>';
+		echo 'จำนวนที่รับมา - '. $value->Amount;
+		echo '<br>';
+		echo 'เมื่อปี - '. $value->Year;
+        echo '<br>';
+		echo '<hr>';
+	}
+
+
+
+	//echo var_dump(json_decode($exec));
